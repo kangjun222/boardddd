7887e146cb8058f74ea80460a2607a6dd6312758
tongw <tongw@DESKTOP-V8BOD71>
2023-11-24 17:58:56 +0900

    게시판 목록 기능 구현 (select문 쿼리 성공)

--- VamPa_Board(Oracle)/src/main/java/com/vam/service/BoardService.java
+++ VamPa_Board(Oracle)/src/main/java/com/vam/service/BoardService.java
@@ -1,7 +1,12 @@
 package com.vam.service;
 
+import java.util.List;
+
 import com.vam.model.BoardVO;
 
 public interface BoardService {
 	public void enroll(BoardVO board);
+	
+	 /* 게시판 목록 */
+    public List<BoardVO> getList();
 }
--- VamPa_Board(Oracle)/src/test/java/com/vam/mapper/BoardMapperTests.java
+++ VamPa_Board(Oracle)/src/test/java/com/vam/mapper/BoardMapperTests.java
@@ -1,5 +1,7 @@
 package com.vam.mapper;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -19,7 +21,7 @@ public class BoardMapperTests {
      @Autowired
      private BoardMapper mapper;
  
-     @Test
+   /*  @Test
      public void testEnroll() {
          
          BoardVO vo = new BoardVO();
@@ -31,10 +33,29 @@ public class BoardMapperTests {
       vo.setWriter("mapper test");
      
          
-         mapper.enroll(vo);
+         mapper.enroll(vo);*/
+     /* 게시판 목록 테스트 */
+     @Test
+     public void testGetList() {
+         
+         
+         List list = mapper.getList();
+        /* 일반적 for문 */
+         for(int i = 0; i < list.size();i++) {
+             log.info("" + list.get(i));
+         }
+         
+        /* foreach문(향상된 for문) */
+         for(Object a : list) {
+             log.info("" + a);
+         }
+         
+        /* foreach문 & 람다식 */
+         list.forEach(board -> log.info("" + board));
+         
+     }
          
      }
      
- 
-}
- 
+
+
