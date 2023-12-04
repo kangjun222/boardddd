c100c13e3c4a75fdf90d52508e71338e5923a794
tongw <tongw@DESKTOP-V8BOD71>
2023-12-04 17:54:49 +0900

    게시판 구현 완료

--- VamPa_Board(Oracle)/src/main/java/com/vam/model/Criteria.java
+++ VamPa_Board(Oracle)/src/main/java/com/vam/model/Criteria.java
@@ -41,6 +41,9 @@ public class Criteria {
 	public String getKeyword() {
 		return keyword;
 	}
+	public void setKeyword(String keyword) {
+		this.keyword=keyword;
+	}
 
 	@Override
 	public String toString() {
